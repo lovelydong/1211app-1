abd38eee433ce6309de481e7b42587892720d07d
熊天赐 <18569952863@163.com>
2018-03-10 17:35:33 +0800

    题库

--- src/pages/questionBank/moduleExam.vue
+++ src/pages/questionBank/moduleExam.vue
@@ -10,41 +10,49 @@
 
         </f7-navbar>
         <f7-subnavbar>
-            <transition
-                  name="custom-classes-transition"
-                  enter-active-class="animated pulse"
-                >
-          <div class="TopsZ" v-if="shows.showTop" >
-            <em></em>
-            <ul>
-              <f7-link><i class="iconfont icon-xiaoxi1"></i> 系统消息</f7-link>
-              <f7-link><i class="iconfont icon-shouye"></i> 返回首页</f7-link>
-              <f7-link><i class="iconfont icon-saoyisao"></i> 扫一扫</f7-link>
-              <f7-link><i class="iconfont icon-gouwuche"></i> 购物车</f7-link>
-              <f7-link  @click="shareboy"><i class="iconfont icon-fenxiang"></i> 分享</f7-link>
-            </ul>
-          </div>
-          </transition>
+        </transition>
             <div class="row">
-                <div class="col-20">类型<i class="iconfont icon-icon--"></i></div>
-                <div class="col-20">学段<i class="iconfont icon-icon--"></i></div>
-                <div class="col-20" :class="{active: shows.showKM}" @click="OnshowKM( $event.target)">科目<i class="iconfont icon-icon--"></i></div>
-                <div class="col-20" style="color:#fd2d44;" @click="shows.showSX = !shows.showSX">筛选<i class="iconfont icon-shaixuan"></i></div>
+                <div class="col-20" :class="{active: shows == 1?true:false}" @click="shows = 1">类型<i class="iconfont icon-icon--"></i></div>
+                <div class="col-20" :class="{active: shows == 2?true:false}" @click="shows = 2">学段<i class="iconfont icon-icon--"></i></div>
+                <div class="col-20" :class="{active: shows == 3?true:false}" @click="shows = 3">科目<i class="iconfont icon-icon--"></i></div>
+                <div class="col-20" style="color:#fd2d44;" @click="shows = 4">筛选<i class="iconfont icon-shaixuan"></i></div>
             </div>
-            <div class="screen"  v-if="shows.showSX" @click="shows.showSX = false">
+            <div class="screen"  v-if="shows == 4?true:false" @click="shows = false">
             </div>
-            <div class="screen1" v-if="shows.showKM" @click="shows.showKM = false">
+            <div class="screen1" v-if="shows == 1?true:false || shows == 2?true:false || shows == 3?true:false" @click="shows = false">
             </div>
             <transition
                   name="custom-classes-transition"
                   enter-active-class="animated bounceInDown"
                   leave-active-class="animated bounceOutUp"
                 >
-              <div class="lx" v-if="shows.showKM">
+              <div class="lx" v-if="shows == 1?true:false">
                   <ul>
-                    <li class="active">教师理论基础 <i class="iconfont icon-selected-copy"></i></li>
-                    <li>学科专业知识 </li>
-                    <li>其他</li>
+                    <li class="col-33" :class="{active: type.type == 2 ?true:false}" @click="type.type = 2 ;typeFn(2)"> 教师招聘 <i v-if="type.type == 2 ?true:false" class="iconfont icon-selected-copy"></i></li>
+                    <li class="col-33" :class="{active: type.type == 3 ?true:false}" @click="type.type = 3 ;typeFn(3)"> 教师资格证 <i v-if="type.type == 3 ?true:false" class="iconfont icon-selected-copy"></i></li>
+                    <li class="col-33" :class="{active: type.type == 4 ?true:false}" @click="type.type = 4 ;typeFn(4)"> 特岗教师 <i v-if="type.type == 4 ?true:false" class="iconfont icon-selected-copy"></i></li>
+                  </ul>
+              </div>
+            </transition>
+             <transition
+                  name="custom-classes-transition"
+                  enter-active-class="animated bounceInDown"
+                  leave-active-class="animated bounceOutUp"
+                >
+              <div class="lx" v-if="shows == 2?true:false">
+                  <ul>
+                    <li v-for="item in gitCon.gradeList" class="col-33" :class="{active: type.gradeList == item.name_value ?true:false}" @click="type.gradeList = item.name_value"> {{item.name}} <i v-if="type.gradeList == item.name_value ?true:false" class="iconfont icon-selected-copy"></i></li>
+                  </ul>
+              </div>
+            </transition>
+             <transition
+                  name="custom-classes-transition"
+                  enter-active-class="animated bounceInDown"
+                  leave-active-class="animated bounceOutUp"
+                >
+              <div class="lx" v-if="shows == 3?true:false">
+                  <ul>
+                    <li v-for="item in gitCon.subjectList" class="col-33" :class="{active: type.subjectList == item.name_value ?true:false}" @click="type.subjectList = item.name_value"> {{item.name}} <i v-if="type.subjectList == item.name_value ?true:false" class="iconfont icon-selected-copy"></i></li>
                   </ul>
               </div>
             </transition>
@@ -53,61 +61,38 @@
                   enter-active-class="animated bounceInRight"
                   leave-active-class="animated bounceOutRight"
                 >
-                  <div class="screen-but" v-if="shows.showSX">
+                  <div class="screen-but" v-if="shows == 4?true:false">
                     <div>
                       <h3>筛选</h3>
-                    <h4>课程类型</h4>
+                    <h4>考试类型</h4>
                     <div class="row">
-                        <div class="col-33">直播课程</div>
-                        <div class="col-33">录播课程</div>
-                        <div class="col-33">回放课程</div>
+                        <div  class="col-33" :class="{active: type.type == 2 ?true:false}" @click="type.type = 2;typeFn(2)">教师招聘</div>
+                        <div  class="col-33" :class="{active: type.type == 3 ?true:false}" @click="type.type = 3;typeFn(3)">教师资格证</div>
+                        <div  class="col-33" :class="{active: type.type == 4 ?true:false}" @click="type.type = 4;typeFn(4)">特岗教师</div>
                     </div>
                     <h4>笔面试</h4>
                     <div class="row">
-                        <div class="col-33">面试</div>
-                        <div class="col-33">笔试</div>
+                        <div v-for="item in gitCon.writeorfaceList" class="col-33" :class="{active: type.writeorfaceList == item.name_value ?true:false}" @click="type.writeorfaceList = item.name_value">{{item.name}}</div>
                     </div>
                     <h4>学段</h4>
                     <div class="row">
-                        <div class="col-33">幼儿</div>
-                        <div class="col-33">小学</div>
-                        <div class="col-33">中学</div>
+                        <div v-for="item in gitCon.gradeList" class="col-33" :class="{active: type.gradeList == item.name_value ?true:false}" @click="type.gradeList = item.name_value">{{item.name}}</div>
                     </div>
                     <h4>科目</h4>
                     <div class="row">
-                        <div class="col-33">教育理论基础</div>
-                        <div class="col-33">学科专业知识</div>
-                        <div   class="col-33">其他</div>
+                        <div v-for="item in gitCon.subjectList" class="col-33" :class="{active: type.subjectList == item.name_value ?true:false}" @click="type.subjectList = item.name_value">{{item.name}}</div>
+
                     </div>
-                    <h4>年份</h4>
-                    <div class="row">
-                        <div class="col-33">2016</div>
-                        <div class="col-33">2017</div>
-                        <div   class="col-33">2018</div>
-                    </div>
+
                     <h4>地区</h4>
                     <div class="row">
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
-                        <div class="col-33">河南省</div>
+                        <div v-for="item in gitCon.areaList" class="col-33" :class="{active: type.areaList == item.name_value ?true:false}" @click="type.areaList = item.name_value">{{item.name}}</div>
                     </div>
                     </div>
                     <div class="off">
                       <div class="row">
-                          <div class="col-50">重置</div>
-                          <div class="col-50">确定</div>
+                          <div class="col-50" @click="type.type =null;type.gradeList =null;type.subjectList =null;type.writeorfaceList =null;type.areaList =null;type.exam_type =null;">重置</div>
+                          <div class="col-50" @click="shows = false">确定</div>
                       </div>
                     </div>
                   </div>
@@ -222,13 +207,20 @@
 export default {
   data: function() {
     return {
-      shows: {
-        showSX: false,
-        showKM: false,
-        showTop: false
+      url: "http://192.168.0.115:8080/shiro_test",
+      shows: false,
+      showTop: false,
+      type: {
+        type: 2,
+        gradeList: null,
+        subjectList: null,
+        writeorfaceList: null,
+        areaList: null
       },
-      classtype:"",
-      qtype:""
+      gitCon: {},
+      recommend: {},
+      classtype: "",
+      qtype: ""
     };
   },
   methods: {
@@ -240,39 +232,128 @@ export default {
     },
     shareboy: function(data) {
       this.$refs.c1.sharefn();
+    },
+    typeFn(e) {
+      this.$http
+        .get(this.url + "/exambank/changearea", {
+          params: {
+            type: e,
+            exam_type: this.qtype
+          }
+        })
+        .then(
+          function(res) {
+            this.gitCon.areaList = res.data.data;
+            console.log(res)
+          },
+          function(res) {
+          }
+        );
     }
   },
-  
-  created:function()
-  {
-  	let url = "http://192.168.0.115:8080/shiro_test";
-  	this.classtype=this.$f7route.query.classtype;
-  	this.qtype=this.$f7route.query.qtype;
-  /*	console.log(this.classtype)
-  	console.log(this.qtype)*/
-  	this.$http
-				.get(url + "/exambank/typelist", {
-					params: {
-						
-						type:this.classtype,
-						exam_type:this. qtype
-					}
-				})
-				.then(
-					function(res) {
-						
-						//this.exams = res.data.data;
-						 console.log(res.data.data)
-					},
-					function(res) {
-						console.log(res.status);
-					});
-  }
- }
 
+  created: function() {
+    let url = "http://192.168.0.115:8080/shiro_test";
+    this.classtype = this.$f7route.query.classtype;
+    this.qtype = this.$f7route.query.qtype;
+    /*	console.log(this.classtype)
+  	console.log(this.qtype)*/
+    this.$http
+      .get(url + "/exambank/typelist", {
+        params: {
+          type: 3,
+          exam_type: this.qtype
+        }
+      })
+      .then(
+        function(res) {
+          this.gitCon = res.data.data;
+        },
+        function(res) {
+        }
+      );
+  }
+};
 </script>
 <style lang="less">
- .subnavbar {
+.moduleExam {
+  .mid {
+    padding: 10px;
+
+    > ul {
+      > li {
+        display: block;
+        padding: 10px 0;
+        min-height: 106px;
+        > .link {
+          display: block;
+
+          > span {
+            width: 120px;
+            height: 86px;
+            background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1519301525135&di=dd2fed90de9f2622da9e2c6f51d3888b&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170922%2Fca414a2cf9b34ec49aff78f350b0ec87.gif)
+              no-repeat center center;
+            border-radius: 12px;
+            position: absolute;
+            left: 0;
+            top: 0;
+          }
+          > div {
+            vertical-align: top;
+            width: 100%;
+            float: left;
+            padding-left: 130px;
+            > p {
+              font-family: "PingFang-SC-Regular";
+              font-size: 16px;
+              color: #333333;
+            }
+            > p:nth-of-type(2) {
+              font-family: "MicrosoftYaHei";
+              font-size: 12px;
+              line-height: 24px;
+              color: #999999;
+              > i {
+                color: #fb7b59;
+                float: right;
+                margin-left: 3px;
+              }
+            }
+            > p:nth-of-type(3) {
+              font-family: "MicrosoftYaHei";
+              font-size: 12px;
+              line-height: 24px;
+              color: #fd2d44;
+            }
+          }
+        }
+      }
+    }
+    > ul {
+      > li {
+        min-height: 70px;
+        > .link {
+          > div {
+            padding: 0;
+            > p:nth-of-type(2) {
+              font-size: 12px;
+              line-height: 28px;
+              > span {
+                font-family: "PingFang-SC-Regular";
+                font-size: 10px;
+                color: #fd5d32;
+                border: 1px solid #fd5d32;
+                padding: 0 2px;
+                border-radius: 5px;
+                margin-right: 5px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .subnavbar {
     .subnavbar-inner {
       > .row {
         height: 100%;
@@ -349,9 +430,15 @@ export default {
           border-bottom: 1px solid #f2f2f2;
           > .col-33 {
             padding: 5px 0;
+            border: 1px solid #f2f2f2;
             background-color: #f2f2f2;
             border-radius: 5px;
             margin: 5px 0;
+          }
+          > .active {
+            border-color: #fd2d44;
+            color: #fd2d44;
+            background-color: #fff;
           }
         }
       }
@@ -402,83 +489,6 @@ export default {
         }
         > .active {
           color: #fd2d44;
-        }
-      }
-    }
-  }
-.moduleExam {
-  .mid {
-    padding: 10px;
-
-    > ul {
-      > li {
-        display: block;
-        padding: 10px 0;
-        min-height: 106px;
-        > .link {
-          display: block;
-
-          > span {
-            width: 120px;
-            height: 86px;
-            background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1519301525135&di=dd2fed90de9f2622da9e2c6f51d3888b&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170922%2Fca414a2cf9b34ec49aff78f350b0ec87.gif)
-              no-repeat center center;
-            border-radius: 12px;
-            position: absolute;
-            left: 0;
-            top: 0;
-          }
-          > div {
-            vertical-align: top;
-            width: 100%;
-            float: left;
-            padding-left: 130px;
-            > p {
-              font-family: "PingFang-SC-Regular";
-              font-size: 16px;
-              color: #333333;
-            }
-            > p:nth-of-type(2) {
-              font-family: "MicrosoftYaHei";
-              font-size: 12px;
-              line-height: 24px;
-              color: #999999;
-              > i {
-                color: #fb7b59;
-                float: right;
-                margin-left: 3px;
-              }
-            }
-            > p:nth-of-type(3) {
-              font-family: "MicrosoftYaHei";
-              font-size: 12px;
-              line-height: 24px;
-              color: #fd2d44;
-            }
-          }
-        }
-      }
-    }
-    > ul {
-      > li {
-        min-height: 70px;
-        > .link {
-          > div {
-            padding: 0;
-            > p:nth-of-type(2) {
-              font-size: 12px;
-              line-height: 28px;
-              > span {
-                font-family: "PingFang-SC-Regular";
-                font-size: 10px;
-                color: #fd5d32;
-                border: 1px solid #fd5d32;
-                padding: 0 2px;
-                border-radius: 5px;
-                margin-right: 5px;
-              }
-            }
-          }
         }
       }
     }
