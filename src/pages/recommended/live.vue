d1ed23a0c6d0ab0737a1cc56bab0f59ee555af04
lovelydong <1476848198@qq.com>
2018-03-13 21:07:47 +0800

    手机号修改，直播配置

--- src/pages/recommended/live.vue
+++ src/pages/recommended/live.vue
@@ -4,11 +4,11 @@
           <f7-nav-left sliding><f7-link class="iconfont icon-zuo" @click="$f7router.back()"></f7-link></f7-nav-left>
           <f7-nav-title>在线播放</f7-nav-title>
         </f7-navbar>
-        <iframe src="http://localhost:8080//shiro_test/sx1211courseAdmin/mobile_live?id=6" frameborder="0" width="100%" height="100%"  id="studyManageIframe"></iframe>
+        <iframe src="http://localhost:8080//shiro_test/sx1211courseAdmin/mobile_live?&uid=5583122&ownerid='zFiZQhs0Sh'&uname='李冬冬'&authcode=534577" frameborder="0" width="100%" height="100%"  id="studyManageIframe"></iframe>
   </f7-page>
 </template>
 <script>
-
+//&ownerid=zFiZQhs0Sh&uname=李冬冬&authcode=534577
 export default {
   data: function() {
     return {
